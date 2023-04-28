c855c10b34088112a4f2de53272d744c9fa37307
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-04-27 21:29:42 -0300

    inserindoTesteAutomatizado

--- src/pages/js/index.js
+++ src/pages/js/index.js
@@ -1,10 +1,9 @@
 function realizarLogin() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
-
     if (login == "admin" && senha == "admin") {
         alert("Login realizado com sucesso!")
-    }else{
+    } else {
         alert("Usuário/senha inválidos.")
     }
 }

7516eda13268ce724197b4fb43c324b1d3d20921
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-05-01 15:34:03 -0300

    paginaCompleta

--- src/pages/js/index.js
+++ src/pages/js/index.js
@@ -2,16 +2,12 @@ function realizarLogin() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
     if (login == "admin" && senha == "admin") {
-        alert("Login realizado com sucesso!")
+        window.location = "home.html";
     } else {
         alert("Usuário/senha inválidos.")
     }
 }
 
 function mensagemPadrao(){
-    alert('A página de esqueceu a sua senha, ainda não está pronta, volte mais tarde!')
-}
-
-function cadastroNovoUsuario(){
-    alert('A página de cadastro de novo usuário, ainda não está pronta, volte mais tarde!')
+    alert('A página que você está tentando acessar, ainda não está pronta, volte mais tarde.')
 }

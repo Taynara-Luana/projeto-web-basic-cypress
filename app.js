c7a815134ef87a09f57287154d688933ef1d1269
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-05-08 21:38:38 -0300

    configurando_docker_correto

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.use(express.static('src'));
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/src/index.html");
+    res.sendFile(__dirname + "/src/pages/index.html");
 })
 
 app.listen(port, () => {

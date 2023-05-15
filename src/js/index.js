a07bdfc44e62cd791695bdf941d3e0fd8ab699e7
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-05-14 21:57:17 -0300

    implementando_codigo

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,3 @@
-function validateField(){
-    const email = document.getElementById('login').value;
-    const senha = document.getElementById('senha').value;
-    if(!email  && !senha){
-        document.getElementById('btnLogin').disabled = true;
-    }else {
-        document.getElementById('btnLogin').disabled = false;
-    }
-}
-
-function realizarLogin() {
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-    if (login == "admin" && senha == "admin") {
-        abrePaginaPrincipal()
-    } else {
-        alert("Usuário/senha inválidos.")
-    }
-}
-
 function mensagemPadrao(){
     alert('A página que você está tentando acessar, ainda não está pronta, volte mais tarde.')
 }

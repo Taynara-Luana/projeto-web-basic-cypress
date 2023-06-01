d78d38750ff87fb261afa12f703f03d5dd6bd11a
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-06-01 13:35:39 -0300

    completo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const usuario = document.getElementById('usuario');
+const senha = document.getElementById('senha');
+
+
+function realizaLoginValido(){
+    if(usuario.value === 'admin' & senha.value ==='123'){
+        window.location = 'home.html'
+    }else{
+        alert('Usuário ou senha inválidos.')
+    }
+}
+
 function mensagemPadrao(){
     alert('Essa funcionalidade ainda não está pronta!');
 }

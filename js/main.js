4d3b32dd535a1cdbd514f8f18203dbe535d30b72
Taynara Luana <taynaraluana@MacBook-Air-de-Taynara.local>
2023-06-15 09:18:15 -0300

    inserindo_mascara_telefone

--- js/main.js
+++ js/main.js
@@ -13,3 +13,15 @@ function realizaLoginValido(){
 function mensagemPadrao(){
     alert('Essa funcionalidade ainda não está pronta!');
 }
+//Validação da mascara de telefone
+const telefone = document.getElementById('telefone') // Seleciona o campo telefone do campo de telefone
+telefone.addEventListener('keypress', (e) => mascaraTelefone(e.target.value)) // Dispara quando digitado no campo
+telefone.addEventListener('change', (e) => mascaraTelefone(e.target.value)) // Dispara quando autocompletado o campo
+
+const mascaraTelefone = (valor) => {
+    valor = valor.replace(/\D/g, "")
+    valor = valor.replace(/^(\d{2})(\d)/g, "($1) $2")
+    valor = valor.replace(/(\d)(\d{4})$/, "$1-$2")
+    telefone.value = valor // Insere o(s) valor(es) no campo
+}
+// Fim da Validação da mascara de telefone
